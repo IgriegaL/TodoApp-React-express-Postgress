158c62be03eccebc87d7dda11f8fe5a127175103
IgriegaL <yehudilara@hotmail.com>
2023-03-11 11:04:13 -0300

    act funciones backend

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,28 +3,30 @@ import { useState } from "react";
 
 function Modal({ mode, setShowModal, task }) {
   const editMode = mode === "edit" ? true : false;
-  console.log('Task :  ---> ' + task)
+  console.log("Task :  ---> " + task);
   const [data, setData] = useState({
     // si viene el email = task.user_email si no, null
-    user_email: editMode ? task.user_email : 'bob@gmail.com',
+    user_email: editMode ? task.user_email : "bob@gmail.com",
     title: editMode ? task.title : null,
     progress: editMode ? task.progress : 50,
     date: editMode ? "" : new Date(),
   });
-
+  
+  console.log('DATA ::: ' + data)
   const postData = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const response = await fetch('http://localhost:8000/todos', {
-        method:"POST",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-      })
-      console.log(response) 
+      const response = await fetch("http://localhost:8000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      //const responseData = await response.json();
+      //console.log('Response : ', response.body);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -44,13 +46,7 @@ function Modal({ mode, setShowModal, task }) {
       <div className="modal">
         <div className="form-title-container">
           <h3>Lets {mode} you task</h3>
-          <button
-            onClick={
-              handleCloseModal
-            }
-          >
-            x
-          </button>
+          <button onClick={handleCloseModal}>x</button>
         </div>
         <form>
           <input
@@ -73,7 +69,11 @@ function Modal({ mode, setShowModal, task }) {
             value={data.progress}
             onChange={handleChange}
           ></input>
-          <input className={mode} type="submit" onClick={editMode ? '' : postData}></input>
+          <input
+            className={mode}
+            type="submit"
+            onClick={editMode ? "" : postData}
+          ></input>
         </form>
       </div>
     </div>
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express();
 const pool = require("./db");
 
 app.use(cors());
+app.use(express.json()); // Agregando el middleware body-parser
 
 // get all todos
 app.get("/todos/:userEmail", async (req, res) => {
@@ -22,21 +23,32 @@ app.get("/todos/:userEmail", async (req, res) => {
   }
 });
 
-// Create one toDO
-app.post('/todos', (req,res) => {
+app.post('/todos', async (req, res) => {
+  const { user_email, title, progress, date } = req.body;
 
-  const {user_email,title,progress,date} = req.body
-  console.log(' *** user_email,title,progress,date ->' + user_email,title,progress,date)
-  // Hacer un nuevo id
-  const id = uuidv4()
-  try {
-    pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1,$2,$3,$4,$5)`,
-    // Front 
-    [id,user_email,title,progress, date]
-    )
-  } catch (error) {
-    console.log(error)
+  // Validar que los datos obligatorios estén presentes
+  if (!user_email || !title || !progress || !date) {
+    return res.status(400).send('Faltan datos obligatorios' + user_email);
   }
-})
+
+  // Validar el formato de los datos
+  if (typeof user_email !== 'string' || typeof title !== 'string' || typeof progress !== 'number' || isNaN(Date.parse(date))) {
+    return res.status(400).send('Los datos no tienen el formato adecuado');
+  }
+
+  const id = uuidv4();
+
+  try {
+    await pool.query(
+      `INSERT INTO todos(id, user_email, title, progress, date) VALUES($1,$2,$3,$4,$5)`,
+      [id, user_email, title, progress, date]
+    );
+    res.send('El todo se ha creado correctamente');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Ha ocurrido un error al crear el todo');
+  }
+});
 
 app.listen(PORT, () => console.log(`Server corriendo en ${PORT}`));
+
